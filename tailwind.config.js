36bdf299322d3d8fd622c3e3797d61fa724fe7cb
Zarha Buske <zarhabuske@hotmail.com>
2024-12-10 15:43:29 +0100

    custom colors, animations, and font settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#011132",
+        secondary: "#014CE5",
+      },
+      keyframes: {
+        slideInOut: {
+          "0%, 100%": { transform: "translateX(100%)", opacity: "0" },
+          "10%, 30%": { transform: "translateX(0)", opacity: "1" },
+          "50%": { transform: "translateX(-100%)", opacity: "0" },
+        },
+      },
+      animation: {
+        "slide-in-out": "slideInOut 15s ease infinite",
+      },
+      fontFamily: {
+        sans: ["Mona Sans", "sans-serif"],
+      },
+
+      boxShadow: {
+        "blue-light": " 0px 0px 25px -10px rgba(10,64,173,1)",
+      },
+    },
   },
   plugins: [],
 };
